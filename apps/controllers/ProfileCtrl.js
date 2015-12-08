554ab42e5292139b821166fb9ca279bfe3131295
Micah Wells <Micah@Micahs-MacBook-Pro.local>
2015-12-08 10:20:01 -0600

    User pins showing on users profile page

--- apps/controllers/ProfileCtrl.js
+++ apps/controllers/ProfileCtrl.js
@@ -1,15 +1,20 @@
-app.controller("ProfileCtrl", ["$scope", "$location", "$firebaseObject", "Auth", "storage",
-  	function($scope, $location, $firebaseObject, Auth, storage) {
+app.controller("ProfileCtrl", ["$scope", "$location", "$firebaseObject", "Auth", "storage", "$firebaseArray",
+  	function($scope, $location, $firebaseObject, Auth, storage, $firebaseArray) {
   		var stupid;
   		// Getting UserID
   		var uid = storage.getUserId();
   		console.log("uid", uid);
 
-	
+		// Firebase ref for Pins
+		var pinsRef = new Firebase("https://sam-pinterest.firebaseio.com/users/" + uid + "/userpins")
+		$scope.userpins = $firebaseArray(pinsRef);
+		console.log("userpins", $scope.userpins);
+
+
 		$scope.addImage = function() {
   			var url = $scope.image;
   			console.log("image", $scope.image);
-  			var pinsRef = new Firebase("https://sam-pinterest.firebaseio.com/users/" + uid)
+  			var pinsRef = new Firebase("https://sam-pinterest.firebaseio.com/users/" + uid + "/userpins")
 			$scope.data = $firebaseObject(pinsRef);
 			console.log("ProfileCtrl click");
 			console.log("scope.data", $scope.data);
