f84cda66857c05b8832ad980b24861cd4dcbc09c
schmitz1193 <schmitz1193@gmail.com>
2015-12-09 09:06:12 -0600

    added comments for ssk

--- apps/controllers/ProfileCtrl.js
+++ apps/controllers/ProfileCtrl.js
@@ -4,19 +4,20 @@ app.controller("ProfileCtrl", ["$scope", "$location", "$firebaseObject", "Auth",
   		var uid = storage.getUserId();
   		console.log("uid", uid);
   		var ref = new Firebase("https://sam-pinterest.firebaseio.com");
-  		// Logout
+  		// Logout --on the nav bar
 	    $scope.logout = function(){
 	      Logout();
 	      console.log("logged out");
 	    };
-	    // Deleting 
+
+	    // Deleting ---- is this being used or called???
 		$scope.delete = function(pin) {
 			console.log("pin", pin);
 			console.log("user pin delete", $scope.userpins);
 			$scope.userpins.$remove(pin);
 		}
 
-
+		// input a URL image and "add" to the users profile
 		$scope.addImage = function() {
 			var url = $scope.image;
 			// Firebase ref for all Pins
@@ -29,6 +30,9 @@ app.controller("ProfileCtrl", ["$scope", "$location", "$firebaseObject", "Auth",
 					uid: uid
 				})	
 		}
+
+	// creating a filtered array from ALL the pins in our pins to get only the pins associated
+	// with the logged in user...to show their pins only in their profile
 
 	$scope.filteredArray = [];
     var pinsRef = new Firebase("https://sam-pinterest.firebaseio.com/pins");
@@ -43,6 +47,14 @@ app.controller("ProfileCtrl", ["$scope", "$location", "$firebaseObject", "Auth",
             })
             return $scope.filteredArray;
         });
+
+        // in the firebase array of pins, use orderByChild to go through array 
+        // and compare the "uid" to the logged in user's uid. 
+        // If it is equal, the child with the matching "uid" is returned to query.
+        // query is used to create the new firebase array of pins with the user's id
+        // ???what is query?  Is it an array???
+        // (need to create array through firebase to use $remove)
+        // finally use the remove on this array to delete the "pin" from the delete button
 
 		var newRef = new Firebase("https://sam-pinterest.firebaseio.com/pins/");
         $scope.filtered = $firebaseArray(newRef);
